1b5c80a5c9e512e8204e7dc26cf7d967e237cb3c
BeanFactory <98367091+aadiujoshi@users.noreply.github.com>
2022-10-18 16:25:01 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+// document.getElementById("header-pane").onclick = function() {
+//     let n = parseInt(document.getElementById("header-pane").innerHTML)+1;
+//     document.getElementById("header-pane").innerHTML = n;
+// }
